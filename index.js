599a9472cce506834d11b39b0865ab0b59a4a443
ShohagKaisar <mshohag.dev@gmail.com>
2025-01-16 19:24:04 +0600

    add user admin making patch api code

--- index.js
+++ index.js
@@ -31,6 +31,31 @@ async function run() {
     const reviewCollection = client.db("bistrodb").collection("reviews");
     const cartCollection = client.db("bistrodb").collection("carts");
     // users related API
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    // Change User Role
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    // Delete user API
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if not exist
